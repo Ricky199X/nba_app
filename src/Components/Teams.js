0b0ac47e8d6d6e3d4db71c9dfee1037221df27b0
Ricky199X <rickyrojas19@gmail.com>
2020-07-18 01:07:40 -0400

    team logos + names rendering. added light styling to the page for structure

--- src/Components/Teams.js
+++ src/Components/Teams.js
@@ -2,13 +2,12 @@ import React from 'react'
 
 class Teams extends React.Component {
 
-
     state = {
         nbaTeams: []
     }
 
 
-    // fetch call to api - sets the 
+    // fetch call to api - sets the team data to state
     async componentDidMount() {
         const url = `https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=NBA`
         let response = await fetch(url)
@@ -16,14 +15,35 @@ class Teams extends React.Component {
         this.setState({ nbaTeams: data.teams })
     }
 
+    renderTeams = () => {
+        return (
+            <div class="row">
+                <ul>
+                    {this.state.nbaTeams.map(element => (
+                        <div className="col s2">
+                            <div className="team-badge">
+                                <li>
+                                    <img src={element.strTeamBadge} className="logo" />
+                                </li>
+                            </div>
+
+                            <div className="team-name">
+                                <li>{element.strTeam}</li>
+                            </div>
+                        </div>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 
 
 
     render() {
-        console.log(this.state.nbaTeams)
+
         return (
-            <div>
-                <h1>Teams Component</h1>
+            <div className="teams-container">
+                {this.renderTeams()}
             </div>
         )
     }
